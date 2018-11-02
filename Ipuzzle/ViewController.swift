4d6432ea625d5bbe97cfac18a076b5b3c9e17c5a
asge <asmow@yahoo.com>
2018-11-01 20:35:54 -0500

    ipuzzle complete with adding Upload button .

--- Ipuzzle/ViewController.swift
+++ Ipuzzle/ViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
-    let questionImageArray = [#imageLiteral(resourceName: "Layer 1"), #imageLiteral(resourceName: "Layer 3"), #imageLiteral(resourceName: "Layer 6"), #imageLiteral(resourceName: "Layer 4"), #imageLiteral(resourceName: "Layer 5"), #imageLiteral(resourceName: "Layer 7"), #imageLiteral(resourceName: "Layer 8"), #imageLiteral(resourceName: "Layer 9"), #imageLiteral(resourceName: "Layer 10")]
-    let correctAns = [0,3,1,4,2,5,6,7,8]
-    var wrongAns = Array(0..<9)
+    let questionImageArray = [#imageLiteral(resourceName: "Layer 1.jpg"),#imageLiteral(resourceName: "Layer 2.jpg"),#imageLiteral(resourceName: "Layer 3.jpg"),#imageLiteral(resourceName: "Layer 4.jpg"),#imageLiteral(resourceName: "Layer 5.jpg"),#imageLiteral(resourceName: "Layer 6.jpg"),#imageLiteral(resourceName: "Layer 7.jpg"),#imageLiteral(resourceName: "Layer 8.jpg"),#imageLiteral(resourceName: "Layer 9.jpg"),#imageLiteral(resourceName: "Layer 10.jpg"),#imageLiteral(resourceName: "Layer 11.jpg"),#imageLiteral(resourceName: "Layer 12.jpg"),#imageLiteral(resourceName: "Layer 13.jpg"),#imageLiteral(resourceName: "Layer 14.jpg"),#imageLiteral(resourceName: "Layer 15.jpg"),#imageLiteral(resourceName: "Layer 16.jpg")]
+    let correctAns = [0,1,2,3,4,5,6,7,8,10,11,12,13,14,15,16]
+    var wrongAns = Array(0..<17)
     var wrongImageArray=[UIImage]()
     var undoMovesArray = [(first: IndexPath, second: IndexPath)]()
     var numberOfMoves = 0
@@ -22,7 +22,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Puzzle"
+        self.title = "IPuzzle"
         self.navigationController?.navigationBar.isTranslucent = false
         
         wrongImageArray = questionImageArray
@@ -61,7 +61,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func restartGame() {
         self.undoMovesArray.removeAll()
-        wrongAns = Array(0..<9)
+        wrongAns = Array(0..<17)
         wrongImageArray = questionImageArray
         firstIndexPath = nil
         secondIndexPath = nil
@@ -91,7 +91,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     //MARK: CollectionView
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 9
+        return 17
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -125,7 +125,8 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width = collectionView.frame.width
-        return CGSize(width: width/3, height: width/3)
+        _ = collectionView.frame.height
+        return CGSize(width: width/4, height: width/4)
     }
     
     func setupViews() {
@@ -170,13 +171,6 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         cv.allowsMultipleSelection = true
         cv.translatesAutoresizingMaskIntoConstraints=false
         return cv
-    }()
-    let btnUpload: UIButton = {
-        let btn=UIButton(type: .system)
-        btn.setTitle("Upload", for: .normal)
-        btn.titleLabel?.font = UIFont.systemFont(ofSize: 20)
-        btn.translatesAutoresizingMaskIntoConstraints=false
-        return btn
     }()
     
     let btnSwap: UIButton = {
